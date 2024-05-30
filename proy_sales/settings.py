3e73444fd9195df5b5ff23963324640569598503
Randy <rmedinan@unemi.edu.ec>
2024-05-30 13:39:16 -0500

    XD7

--- proy_sales/settings.py
+++ proy_sales/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import dj_database_url
 import os
 from pathlib import Path
 
@@ -69,25 +68,16 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Proyecto_Autentication.wsgi.application'
+WSGI_APPLICATION = 'proy_sales.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'factur.sqlite3',
-#     }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
-
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'factur.sqlite3',
+     }
+ }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -121,7 +111,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/' # url de archivos estaticos
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)#carpeta fisica de archivos estaticos
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/' # url de imagenes
 LOGIN_URL = '/login'
 
